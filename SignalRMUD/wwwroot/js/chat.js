2dfa0e3f769df9825458f9df2e4721e49454d87a
Aleksi Alajoki <fensnake@yahoo.com>
2019-11-07 21:35:22 +0200

    Application base w. functional chat

---
+++ SignalRMUD/wwwroot/js/chat.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/mainHub").build();
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
